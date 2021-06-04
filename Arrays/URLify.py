50da8a56f0d4405701e0c19a2e33c5eb4b2e3264
walikhan10 <walikhan10@gmail.com>
2021-06-03 23:45:12 -0400

    Update URLify.py

--- Arrays/URLify.py
+++ Arrays/URLify.py
@@ -13,3 +13,18 @@ def URLify(str):
 
 
 print(URLify('Wali Khan'))
+
+
+def URLify2(str):
+
+    url = " "
+
+    for i in str:
+        if i == " ":
+            url += '%20'
+        else:
+            url += i
+    return url
+
+
+print(URLify2('Wali Khan'))
