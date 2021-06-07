ecb410a9925af76df33d69fd4766e10a72d596e0
walikhan10 <walikhan10@gmail.com>
2021-06-06 21:44:09 -0400

    Update rev_string.py

--- Arrays/rev_string.py
+++ Arrays/rev_string.py
@@ -14,7 +14,51 @@ def rev_string(s):
     return list(s2)
 
 
-test = ["y", "e", "l", "l", "o"]
+#test = ["y", "e", "l", "l", "o"]
 
 
-print(rev_string(test))
+# print(rev_string(test))
+
+
+def rev_string2(s):
+
+    l = 0
+    r = len(s)-1
+
+    while l < r:
+
+        # s[l], s[r] = s[r], s[l]
+        # l += 1
+        # r -= 1
+
+        temp = s[l]
+        s[l] = s[r]
+        s[r] = temp
+
+        l += 1
+        r -= 1
+
+    return s
+
+
+test = ["h", "e", "l", "l", "o"]
+
+print(rev_string2(test))
+
+
+def reverseString(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    string = list(s)
+    i, j = 0, len(string) - 1
+    while i < j:
+        string[i], string[j] = string[j], string[i]
+        i += 1
+        j -= 1
+    return "".join(string)
+
+
+# test = ["y", "e", "l", "l", "o"]
+# print(reverseString(test))
